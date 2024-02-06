9168503fec55876f98052f9db4761fbc150ce0ab
Benjamin O'Neill <tun49498@temple.edu>
2024-02-06 11:41:05 -0500

    Created a val numberArray Created a adapter for ArrayAdapter, and spinner.adapter

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    private fun ArrayAdapter(content: Any, mainActivity: MainActivity, simpleDropdownItem1line: Int, numberArray: Array<Int>): Any {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +21,8 @@ class MainActivity : AppCompatActivity() {
         val numberArray = (2 ..  100 step 2).toList().toTypedArray()
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
+        spinner.adapter = adapter as SpinnerAdapter?
 
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
